fb8d52dd3d3f5b5f9b327a1571ebdc477b53ef64
kristeltammesalu <kristel.tammeslau@gmail.com>
2015-09-06 20:49:03 +0300

    Other pages, basic setup and design

--- main.js
+++ main.js
@@ -13,3 +13,37 @@ $(window).scroll(function() {
 $(document).ready(function(){
     $(".parallax").parallax();
 });
+
+$(window).scroll(function(){
+    $(".top").css("opacity", 1 - $(window).scrollTop() / 250);
+});
+
+$('#link').click(function(){
+    $("html, body").animate({ scrollTop: 700 });
+    return false;
+});
+
+
+//lisa
+
+new scrollReveal();
+
+$(document).ready(function(){
+    $('.scrollspy').scrollSpy();
+});
+
+$('.dropdown-button').dropdown({
+        inDuration: 300,
+        outDuration: 225,
+        constrain_width: true, // Does not change width of dropdown to that of the activator
+        hover: true, // Activate on hover
+        gutter: 0, // Spacing from edge
+        belowOrigin: true // Displays dropdown below the button
+    }
+);
+
+// Init Skrollr
+var s = skrollr.init();
+
+// Refresh Skrollr after resizing our sections
+s.refresh($('.homeSlide'));
